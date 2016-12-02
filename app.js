59b4e1b89170e16a1ccb581e2a2205d00cec32d8
scooterblume <scooterblume@yahoo.com>
2016-12-01 19:03:59 -0500

    express generator

--- app.js
+++ app.js
@@ -1,41 +1,46 @@
 var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
 
-app.set('port', (process.env.PORT || 5000));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static('public'));
-// server code goes here!
+app.use('/', index);
+app.use('/users', users);
 
-var tear =0;
-var scream =0;
-
-io.on('connection',function(socket){
-	console.log('connected');
-	socket.emit('tick')
-
-	socket.on('scream',function(){
-		console.log('scream clicked')
-		scream+=10;
-		socket.emit('movScr',scream);
-	});
-
-	socket.on('tear',function(){
-		console.log('tears clicked')
-		tear += 10;
-		socket.emit('movTear',tear);
-	});
-
-	socket.on('tick',function(){
-		var info = [tear,scream];
-		socket.emit('tick', info);
-	});
-
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
